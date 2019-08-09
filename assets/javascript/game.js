a0c0c0dc0d7f5a3fb8bbe2dce1c58c109ffa9912
mbolters <mariematisans@gmail.com>
2019-08-09 12:46:29 -0500

    add steps to code

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Testing JavaScript</title>
+</head>
+<body>
+
+<script type="text/javascript">
+Your JavaScript code goes here.
+</script>
+
+</body>
+</html>
+
